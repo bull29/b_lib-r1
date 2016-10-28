ce5272690be821aef9e0dedc1a9ce40abd8e6635
Bull <thebull2140@gmail.com>
2016-10-28 17:03:53 -0400

    i really need to learn how to use git better

--- lua/b_lib/hook.lua
+++ lua/b_lib/hook.lua
@@ -6,6 +6,7 @@ function hook.RunOnce(str, func)
     hooknum = hooknum + 1
     local c = "temporary_hook_" .. hooknum
 
+    hook.Add( str, c, function( ... )
         func( ... )
         hook.Remove(str, c)
     end)
