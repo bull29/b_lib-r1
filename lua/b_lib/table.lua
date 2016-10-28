d8c3a0ad20239fb8aa77f7b235ff35f00d801347
Bull <thebull2140@gmail.com>
2016-10-28 18:28:30 -0400

    table.rlen

--- lua/b_lib/table.lua
+++ lua/b_lib/table.lua
@@ -1,7 +1,6 @@
 -- tableshit
 
 -- originally made for a permissions addon I was working on, I could quickly pop branching default permissions without much hassle
-
 function table.ibranch(tab, fill, ...)
     local args = {...}
     tab[args[1]] = tab[args[1]] or {}
@@ -16,4 +15,15 @@ function table.ibranch(tab, fill, ...)
 
         c = c[args[i]]
     end
+end
+
+-- recursive length of a table
+function table.rlen( tab )
+    if not istable( tab ) then return 0 end
+    local n = 0
+    for k, v in pairs( tab ) do
+        n = n + 1
+        n = n + table.rlen( v )
+    end
+    return n
 end
