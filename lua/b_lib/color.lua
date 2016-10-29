344ae5c55b39a0a094ed170332ce368cbe3a6185
Bull <thebull2140@gmail.com>
2016-10-28 20:05:11 -0400

    incrementcol

--- lua/b_lib/color.lua
+++ lua/b_lib/color.lua
@@ -4,3 +4,12 @@
 function negatecol( col )
     return Color( 255 - col.r, 255 - col.g, 255 - col.b, col.a or 255 )
 end
+
+--increment/decrement a color
+function incrementcol( col, am )
+    col.r = col.r + am
+    col.g = col.g + am
+    col.b = col.b + am
+
+    return Color( col.r, col.g, col.b, col.a or 255 )
+end
