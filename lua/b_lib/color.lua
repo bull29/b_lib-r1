ce4e6ae23772ddf50b34dbb862ed226172b45f0d
Bull <thebull2140@gmail.com>
2016-10-29 00:19:27 -0400

    moved color stuff to it's own table

--- lua/b_lib/color.lua
+++ lua/b_lib/color.lua
@@ -1,11 +1,12 @@
 --colorstuff
+col = col or {}
 
 -- negate a color/return the opposite of a color
-function negatecol( col )
+function col.negate( col )
     return Color( 255 - col.r, 255 - col.g, 255 - col.b, col.a or 255 )
 end
 
 --increment/decrement a color
-function incrementcol( col, am )
+function col.increment( col, am )
     return Color( col.r + am, col.g + am, col.b + am, col.a or 255 )
 end
