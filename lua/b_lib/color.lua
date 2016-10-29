8f27395118952b59b7e8485fa168f428e7093ef9
Bull <thebull2140@gmail.com>
2016-10-29 18:32:59 -0400

    un-duplicated binding of col

--- lua/b_lib/color.lua
+++ lua/b_lib/color.lua
@@ -2,13 +2,13 @@
 col = col or {}
 
 -- negate a color/return the opposite of a color
-function col.negate( col )
-    return Color( 255 - col.r, 255 - col.g, 255 - col.b, col.a or 255 )
+function col.negate( c )
+    return Color( 255 - c.r, 255 - c.g, 255 - c.b, col.a or 255 )
 end
 
 --increment/decrement a color
-function col.increment( col, am )
-    return Color( col.r + am, col.g + am, col.b + am, col.a or 255 )
+function col.increment( c, am )
+    return Color( c.r + am, c.g + am, c.b + am, c.a or 255 )
 end
 
 -- random color with optional alpha declaration
